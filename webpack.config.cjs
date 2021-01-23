8bce00c1a23a2e155753fdaec6a4ffd492c6618e
Geoff Williams <geoff@declarativesystems.com>
2021-01-23 16:52:48 +1100

    no babel

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,26 +1,12 @@
 const path = require("path")
 
 module.exports = {
-  target: ['web', 'es5'],
-
   entry: './src/index.js',
+  mode: "production",
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    library: 'fklogger',
-    environment: {
-      // ...
-      arrowFunction: false, // <-- this line does the trick
-    },
+    library: "$",
+    libraryTarget: "umd",
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-    ],
-  },
-  mode: "production",
-}
+};
