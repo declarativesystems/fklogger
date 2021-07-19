fe275a5753a2b73ad1fc3f8851126cda2109fcbc
Geoff Williams <geoff@declarativesystems.com>
2021-07-19 16:22:27 +1000

    crud cleanup

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,56 @@
-// todo write a test suite ;-)
+import {
+  levelDebug, levelInfo, levelWarn, levelError, levelOff, fklogger,
+} from "./index";
+
 beforeEach(async () => {
   // something
 });
 
-test("a test", () => {
-  expect(true).toBeDefined();
+describe("test suite", () => {
+  test("the interface", () => {
+    fklogger.debug("debug message");
+    fklogger.info("info message");
+    fklogger.error("error message");
+    fklogger.warn("warn message");
+  });
+
+  test("levelOff works", () => {
+    fklogger.level = levelOff;
+    expect(fklogger.shouldLog(levelDebug)).toBe(false);
+    expect(fklogger.shouldLog(levelInfo)).toBe(false);
+    expect(fklogger.shouldLog(levelWarn)).toBe(false);
+    expect(fklogger.shouldLog(levelError)).toBe(false);
+  });
+
+  test("levelDebug works", () => {
+    fklogger.level = levelDebug;
+    expect(fklogger.shouldLog(levelDebug)).toBe(true);
+    expect(fklogger.shouldLog(levelInfo)).toBe(true);
+    expect(fklogger.shouldLog(levelWarn)).toBe(true);
+    expect(fklogger.shouldLog(levelError)).toBe(true);
+  });
+
+  test("levelInfo works", () => {
+    fklogger.level = levelInfo;
+    expect(fklogger.shouldLog(levelDebug)).toBe(false);
+    expect(fklogger.shouldLog(levelInfo)).toBe(true);
+    expect(fklogger.shouldLog(levelWarn)).toBe(true);
+    expect(fklogger.shouldLog(levelError)).toBe(true);
+  });
+
+  test("levelWarn works", () => {
+    fklogger.level = levelWarn;
+    expect(fklogger.shouldLog(levelDebug)).toBe(false);
+    expect(fklogger.shouldLog(levelInfo)).toBe(false);
+    expect(fklogger.shouldLog(levelWarn)).toBe(true);
+    expect(fklogger.shouldLog(levelError)).toBe(true);
+  });
+
+  test("levelError works", () => {
+    fklogger.level = levelError;
+    expect(fklogger.shouldLog(levelDebug)).toBe(false);
+    expect(fklogger.shouldLog(levelInfo)).toBe(false);
+    expect(fklogger.shouldLog(levelWarn)).toBe(false);
+    expect(fklogger.shouldLog(levelError)).toBe(true);
+  });
 });
